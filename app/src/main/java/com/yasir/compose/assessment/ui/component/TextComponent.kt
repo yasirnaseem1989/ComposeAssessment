a0392d6a58b3f8de99acc2641eaa0a3c3f617bb1
yasir <yasirnaseem1990@icloud.com>
2024-07-13 19:45:37 +0400

    Fix keyboard options

--- app/src/main/java/com/yasir/compose/assessment/ui/component/TextComponent.kt
+++ app/src/main/java/com/yasir/compose/assessment/ui/component/TextComponent.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -89,7 +90,8 @@ fun UserNameComponent(
             )
         },
         keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Text
+            keyboardType = KeyboardType.Text,
+            imeAction = ImeAction.Done
         ),
         borderColor = borderColor
     ) {
